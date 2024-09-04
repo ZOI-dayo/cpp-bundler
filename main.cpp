14dc0a665d8d3bdfa9eadab6c15b0d7b0d33a72b
ZOI-dayo <zoizoi.dayo@gmail.com>
2024-09-05 02:32:34 +0900

    fix #1

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <stack>
 #include <string>
 #include <set>
 #include <fstream>
@@ -11,6 +12,7 @@ using namespace std;
 namespace fs = filesystem;
 using Path = fs::path;
 
+// ヘッダファイルの絶対パスを探す
 Path lookup_header(const Path include_file, const Path current_file, const deque<Path>& include_path) {
   assert(include_file.is_relative());
   assert(current_file.is_absolute());
@@ -29,26 +31,77 @@ Path lookup_header(const Path include_file, const Path current_file, const deque
   throw runtime_error(format("{}の処理中、{}というヘッダが見つかりませんでした。", current_file.string(), include_file.string()));
 }
 
-void read_file(Path file, const deque<Path>& include_path, set<Path>& skip_target) {
+void read_file(Path file, const deque<Path>& include_path, set<Path>& skip_target, const set<string>& default_define_set) {
   ifstream src_file_reader(file);
   if (!src_file_reader) {
     throw runtime_error("入力ファイルが見つかりませんでした。");
   }
 
-  int num = 1;
+  int line = 1;
+
+  const string include_token = "#include \"";
+  const string define_token = "#define ";
+  const string undef_token = "#undef ";
+  const string if_token = "#if ";
+  const string ifdef_token = "#ifdef ";
+  const string ifndef_token = "#ifndef ";
+  const string endif_token = "#endif";
+  const string else_token = "#else";
+  set<string> define_set(default_define_set);
+  stack<bool> if_stack;
+
   while (!src_file_reader.eof()) {
     string s;
     getline(src_file_reader, s);
-    const string include_token = "#include \"";
+
+    if(s.starts_with(if_token)) {
+      if_stack.push(true);
+      cout << s << "\n";
+      continue;
+    } else if(s.starts_with(ifdef_token)) {
+      if_stack.push(define_set.count(s.substr(ifdef_token.size())));
+      cout << s << "\n";
+      continue;
+    } else if(s.starts_with(ifndef_token)) {
+      if_stack.push(!define_set.count(s.substr(ifndef_token.size())));
+      cout << s << "\n";
+      continue;
+    } else if(s.starts_with(endif_token)) {
+      if(if_stack.empty()) {
+        throw runtime_error(format("{}の処理中、{}が余分に存在します。", file.string(), endif_token));
+      }
+      if_stack.pop();
+      cout << s << "\n";
+      continue;
+    } else if(s.starts_with(else_token)) {
+      if(if_stack.empty()) {
+        throw runtime_error(format("{}の処理中、{}が余分に存在します。", file.string(), else_token));
+      }
+      if_stack.top() = !if_stack.top();
+      cout << s << "\n";
+      continue;
+    }
+
+    if(!if_stack.empty() && !if_stack.top()) {
+      cout << "\n";
+      continue;
+    }
+
     if(s.starts_with(include_token)) {
       const Path include_file = lookup_header(s.substr(include_token.size(), s.size()-(include_token.size()+1)), file, include_path);
       if(skip_target.count(include_file)) {
         cout << "\n";
       } else {
         cout << format("#line 1 \"{}\"\n", include_file.string());
-        read_file(include_file, include_path, skip_target);
-        cout << format("#line {} \"{}\"\n", num+1, file.string());
+        read_file(include_file, include_path, skip_target, default_define_set);
+        cout << format("#line {} \"{}\"\n", line+1, file.string());
       }
+    } else if(s.starts_with(define_token)) {
+      define_set.insert(s.substr(define_token.size()));
+      cout << s << "\n";
+    } else if(s.starts_with(undef_token)) {
+      define_set.erase(s.substr(undef_token.size()));
+      cout << s << "\n";
     } else {
       if(s.starts_with("#pragma once")) {
         skip_target.insert(file);
@@ -57,20 +110,24 @@ void read_file(Path file, const deque<Path>& include_path, set<Path>& skip_targe
         cout << s << "\n";
       }
     }
-    num++;
+    line++;
   }
   cout << flush;
 }
 
 int main(int argc, char** argv) {
   optional<Path> lib_path;
+  set<string> default_define_set;
 
   {
     int opt;
-    while((opt = getopt(argc, argv, "I:")) != -1) {
+    while((opt = getopt(argc, argv, "I:D:")) != -1) {
       switch(opt) {
         case 'I':
           lib_path = optarg;
+          break;
+        case 'D':
+          default_define_set.insert(optarg);
           break;
         default:
           throw runtime_error("不明なオプションが指定されました。");
@@ -93,5 +150,5 @@ int main(int argc, char** argv) {
   }
 
   set<Path> skip_target;
-  read_file(src_file, include_path, skip_target);
+  read_file(src_file, include_path, skip_target, default_define_set);
 }
